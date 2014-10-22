761f5d97f2f30ae8b2ef147137e60a1c134b1b96
Matthew Bourn <mbourn@mail.csuchico.edu>
2014-10-22 05:19:30 +0000

    Finished through step 11

--- ratings/app/models/professor.rb
+++ ratings/app/models/professor.rb
@@ -3,6 +3,10 @@ class Professor < ActiveRecord::Base
   has_many :ratings, dependent: :destroy
   accepts_nested_attributes_for :ratings
 
+  validates :first, presence: true
+  validates :last, presence: true
+  validates :university, presence: true
+
   def name
     first + ' ' + last
   end
