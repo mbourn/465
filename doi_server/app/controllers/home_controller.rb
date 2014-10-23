88558f3384b219b768f95621758949b631d8d993
Matthew Bourn <mbourn@mail.csuchico.edu>
2014-10-23 13:32:45 +0000

    Query function works in the most rudamentary way. Some views cleaned up

--- doi_server/app/controllers/home_controller.rb
+++ doi_server/app/controllers/home_controller.rb
@@ -6,14 +6,7 @@ class HomeController < ApplicationController
 
   def show
     @query_text = params[:query]
-    if @query_text =~ /^[a-fA-F0-9]*$/
-      @result = Doi.select("doi, name").where doi: @query_text
-      if !@result
-        @result = ["Couldn't find that DOI"]
-      end
-    else
-      @result = ["Illegal DOI"]
-    end
+    @result = Doi.where doi: @query_text
   end
 
 end
